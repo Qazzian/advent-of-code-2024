09c699095409c845a958844de7cfde1817854e12
Qazzian <Qazzian@gmail.com>
2024-12-24 19:43:57 +0000

    Added Grid and vector classes

---
+++ src/utils/Vector.ts
@@ -0,0 +1,22 @@
+export type Vector = [number, number];
+
+export const UP = vector(0, -1);
+export const N = UP;
+export const RIGHT = vector(1, 0);
+export const E = RIGHT;
+export const DOWN = vector(0, 1);
+export const S = DOWN;
+export const LEFT = vector(-1, 0);
+export const W = LEFT;
+export const NE = add(N, E);
+export const SE = add(S, E);
+export const SW = add(S, W);
+export const NW = add(N, W);
+
+export function vector(x: number, y: number):Vector {
+	return [x, y];
+}
+
+export function add(a:Vector, b:Vector) {
+	return [a[0]+b[0], a[1]+b[1]];
+}
