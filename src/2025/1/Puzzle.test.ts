6e91916fb04b2f587796061e21c6097569a35d8d
Qazzian <Qazzian@gmail.com>
2025-12-24 09:15:44 +0000

    2025 day 1 pt2

--- src/2025/1/Puzzle.test.ts
+++ src/2025/1/Puzzle.test.ts
@@ -4,6 +4,7 @@ import {
 	expectedFirstSolution,
 	second,
 	expectedSecondSolution,
+	countPasses,
 } from './Puzzle.js';
 import readFile from '../../utils/readFile.js';
 import { describe, expect, test } from 'vitest';
@@ -15,8 +16,25 @@ describe('Day 1', async () => {
 	});
 
 	test('second solution test', async () => {
-		const input = await readTestFile(2);
+		const input = await readTestFile();
 		expect(second(input)).toBe(expectedSecondSolution);
+		const input2 = await readTestFile(2);
+		expect(second(input2)).toBe(expectedSecondSolution + 20);
+	});
+
+	test('countPasses', async () => {
+		expect(countPasses(0, 'L1', 10).count).toBe(0);
+		expect(countPasses(0, 'L10', 10).count).toBe(1);
+		expect(countPasses(0, 'L5', 10).count).toBe(0);
+
+		expect(countPasses(5, 'L1', 10).count).toBe(0);
+		expect(countPasses(5, 'L10', 10).count).toBe(1);
+		expect(countPasses(5, 'L5', 10).count).toBe(1);
+		expect(countPasses(5, 'L20', 10).count).toBe(2);
+		expect(countPasses(5, 'R1', 10).count).toBe(0);
+		expect(countPasses(5, 'R10', 10).count).toBe(1);
+		expect(countPasses(5, 'R5', 10).count).toBe(1);
+		expect(countPasses(5, 'R20', 10).count).toBe(2);
 	});
 
 	async function readTestFile(num: number = 1) {
--- src/2025/1/Puzzle.ts
+++ src/2025/1/Puzzle.ts
@@ -14,7 +14,7 @@ const first = (input: string) => {
 		if (currentPos === 0) {
 			password++;
 		}
-		console.log(dir, count, currentPos, password);
+		// console.log(dir, count, currentPos, password);
 	}
 
 	return password;
@@ -23,9 +23,56 @@ const first = (input: string) => {
 const expectedFirstSolution = 3;
 
 const second = (input: string) => {
-	return 'solution 2';
+	const startPos = 50;
+	const modLimit = 100;
+	const lines = input.split('\n');
+	let currentPos = startPos;
+	let password = 0;
+
+	for (const line of lines) {
+		const { count, nextPos } = countPasses(currentPos, line, modLimit);
+		password += count;
+		// console.log(currentPos, line, nextPos, password);
+		currentPos = nextPos % modLimit;
+		if (currentPos < 0) {
+			currentPos = modLimit + currentPos;
+		}
+	}
+
+	return password;
 };
 
-const expectedSecondSolution = 'solution 2';
+const expectedSecondSolution = 6;
+
+interface CountResult {
+	count: number;
+	nextPos: number;
+}
+
+export function countPasses(
+	startPos: number,
+	turn: string,
+	limit: number
+): CountResult {
+	const dir = turn.slice(0, 1);
+	const count = parseInt(turn.slice(1));
+
+	const fullRotations = Math.floor(count / limit);
+	const partalCount = count - fullRotations * limit;
+
+	if (dir === 'L') {
+		const nextPos = startPos - partalCount;
+		if (startPos > 0 && nextPos <= 0) {
+			return { count: fullRotations + 1, nextPos: nextPos };
+		}
+		return { count: fullRotations, nextPos: nextPos };
+	} else {
+		const nextPos = startPos + partalCount;
+		if (startPos < limit && nextPos >= limit) {
+			return { count: fullRotations + 1, nextPos: nextPos };
+		}
+		return { count: fullRotations, nextPos: nextPos };
+	}
+}
 
 export { first, expectedFirstSolution, second, expectedSecondSolution };
