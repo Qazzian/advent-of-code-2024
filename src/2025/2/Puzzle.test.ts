fcc41fda6f7fa97dcc88724f70955d2003acc6e9
Qazzian <Qazzian@gmail.com>
2025-12-26 23:19:56 +0000

    2025 D2 pt2

--- src/2025/2/Puzzle.test.ts
+++ src/2025/2/Puzzle.test.ts
@@ -5,19 +5,16 @@ import {
 	second,
 	expectedSecondSolution,
 	isInvalid,
+	isInValidPt2,
+	isValidForPart,
 } from './Puzzle.js';
 import readFile from '../../utils/readFile.js';
 import { describe, expect, test } from 'vitest';
 
-describe('Day 1', async () => {
+describe('Day 2', async () => {
 	test('first solution test', async () => {
 		const input = await readTestFile();
 		expect(first(input)).toBe(expectedFirstSolution);
-	});
-
-	test('second solution test', async () => {
-		const input = await readTestFile(2);
-		expect(second(input)).toBe(expectedSecondSolution);
 	});
 
 	test('is invalid', async () => {
@@ -25,6 +22,45 @@ describe('Day 1', async () => {
 		expect(isInvalid(12)).toBe(false);
 		expect(isInvalid(1188511885)).toBe(true);
 		expect(isInvalid(1188511880)).toBe(false);
+	});
+
+	test('second solution test', async () => {
+		const input = await readTestFile();
+		expect(second(input)).toBe(expectedSecondSolution);
+		expect(second('709-872')).toBe(777);
+		expect(second('2487-3128')).toBe(16665);
+		expect(second('709-872,2487-3128')).toBe(777 + 16665);
+		expect(second('2-16')).toBe(11);
+	});
+
+	test('is invalid part 2', async () => {
+		expect(isInValidPt2(1)).toBe(false);
+		expect(isInValidPt2(11)).toBe(true);
+		expect(isInValidPt2(12)).toBe(false);
+		expect(isInValidPt2(111)).toBe(true);
+		expect(isInValidPt2(112)).toBe(false);
+		expect(isInValidPt2(1212)).toBe(true);
+		expect(isInValidPt2(1112)).toBe(false);
+		expect(isInValidPt2(101)).toBe(false);
+		expect(isInValidPt2(123123)).toBe(true);
+		expect(isInValidPt2(123123123)).toBe(true);
+		expect(isInValidPt2(121212)).toBe(true);
+		expect(isInValidPt2(121213)).toBe(false);
+	});
+
+	test('isValidForPart', async () => {
+		expect(isValidForPart('1', 1)).toBe(true);
+		expect(isValidForPart('12', 1)).toBe(true);
+		expect(isValidForPart('11', 1)).toBe(false);
+		expect(isValidForPart('111', 1)).toBe(false);
+		expect(isValidForPart('112', 1)).toBe(true);
+		expect(isValidForPart('1123', 2)).toBe(true);
+		expect(isValidForPart('1212', 2)).toBe(false);
+		expect(isValidForPart('121212', 2)).toBe(false);
+		expect(isValidForPart('121213', 2)).toBe(true);
+		expect(isValidForPart('123123', 3)).toBe(false);
+		expect(isValidForPart('123123123', 3)).toBe(false);
+		expect(isValidForPart('123412345', 5)).toBe(true);
 	});
 
 	async function readTestFile(num: number = 1) {
--- src/2025/2/Puzzle.ts
+++ src/2025/2/Puzzle.ts
@@ -3,7 +3,7 @@ const first = (input: string) => {
 	let total = 0;
 	for (const range of ranges) {
 		const [first, second] = range.split('-');
-		total += testRange(parseInt(first), parseInt(second));
+		total += testRange(parseInt(first), parseInt(second), isInvalid);
 	}
 
 	return total;
@@ -12,17 +12,29 @@ const first = (input: string) => {
 const expectedFirstSolution = 1227775554;
 
 const second = (input: string) => {
-	return 'solution 2';
+	const ranges = input.split(',');
+	let total = 0;
+	for (const range of ranges) {
+		const [first, second] = range.split('-');
+		total += testRange(parseInt(first), parseInt(second), isInValidPt2);
+	}
+
+	return total;
 };
 
-const expectedSecondSolution = 'solution 2';
+const expectedSecondSolution = 4174379265;
 
 export { first, expectedFirstSolution, second, expectedSecondSolution };
 
-export function testRange(start: number, end: number) {
+export function testRange(
+	start: number,
+	end: number,
+	testFunc: (id: number) => boolean
+) {
 	let total = 0;
 	for (let i = start; i <= end; i++) {
-		if (isInvalid(i)) {
+		if (testFunc(i)) {
+			// console.log('+', i);
 			total += i;
 		}
 	}
@@ -31,8 +43,42 @@ export function testRange(start: number, end: number) {
 
 export function isInvalid(id: number) {
 	const str = id.toString();
+	if (str.length === 1) {
+		return false;
+	}
+
 	const p1 = str.slice(0, str.length / 2);
 	const p2 = str.slice(str.length / 2);
 
 	return p1 === p2;
+}
+
+export function isInValidPt2(id: number) {
+	const str = id.toString();
+	if (str.length === 1) {
+		return false;
+	}
+	const halfLength = Math.ceil(str.length / 2);
+
+	for (let partLength = 1; partLength <= halfLength; partLength++) {
+		const partResult = isValidForPart(str, partLength);
+		if (!partResult) {
+			return true;
+		}
+	}
+	return false;
+}
+
+export function isValidForPart(str: string, partLength: number): boolean {
+	if (partLength * 2 > str.length) {
+		return true;
+	}
+	const pt1 = str.slice(0, partLength);
+	for (let i = partLength; i < str.length; i += partLength) {
+		const pt2 = str.slice(i, i + partLength);
+		if (pt1 !== pt2) {
+			return true;
+		}
+	}
+	return false;
 }
